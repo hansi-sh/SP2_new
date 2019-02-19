b059f58e8d2895bbccedb0ef69f3c9109b5545ee
Meben <Melvanchanruennkai@gmail.com>
2019-02-19 14:00:31 +0800

    Added camera 3 and physics

--- DM2126Prac/Source/Camera3.h
+++ DM2126Prac/Source/Camera3.h
@@ -7,16 +7,19 @@
 
 class Camera3 : public Camera
 {
-public:
-
+private:
 	Vector3 defaultPosition;
 	Vector3 defaultTarget;
 	Vector3 defaultUp;
+	Vector3 offSet;
+	Mtx44 rotateArndObj;
+
+public:
 
 	Camera3();
 	~Camera3();
 	virtual void Init(const Vector3& pos, const Vector3& target, const Vector3& up);
-	virtual void Update(float degree, float x, float y, float z); //third person camera should take it X and Z position of player to update it and degree to rotate camera by
+	virtual void Update(float degree, float x, float y, float z);
 	virtual void Reset();
 };
 
