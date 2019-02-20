3c93f4620fc1ea76bfa6d7781a735972db8fd832
RaphaelFong <raphaelloveanime@gmail.com>
2019-02-20 17:35:41 +0800

    Added interactions in Ambulance Scene

--- DM2126Prac/Source/Application.cpp
+++ DM2126Prac/Source/Application.cpp
@@ -131,7 +131,8 @@ void Application::Run()
 	}
 	else // change back to PuzzleRoom when pushing 
 	{
-		scene = new AssignmentScene();
+		 scene = new Scene2();
+		 glfwSetCursorPosCallback(m_window, Scene2::mouse_callback);// when ever the cursor moves, this function will be called
 		//glfwSetCursorPosCallback(m_window, AssignmentScene::mouse_callback);// when ever the cursor moves, this function will be called
 	}
 	
